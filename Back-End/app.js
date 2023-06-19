ede729425104b9ee1992c3e6c77217e0293bfeab
Baptiste Ribot <baptisteribot@outlook.fr>
2023-06-19 15:21:08 +0200

    ajout middleware du cours

--- Back-End/app.js
+++ Back-End/app.js
@@ -2,23 +2,43 @@ const express = require('express');
 
 const app = express();
 
-app.use ((req, res, next) => {
-    console.log('Requête reçue !');
-    next();
-});
-
-app.use((req, res, next) =>{
-    res.status(201);
-    next();
-});
-
 app.use((req, res, next) => {
-    res.json({message : 'Votre requête a bien été reçue !'});
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-});
+  });
 
-app.use((req, res) =>{
-    console.log('Réponse envoyée avec succès !');
-});
+app.use('/api/books', (req, res, next) => {
+    const stuff = [
+      {
+        id: "1",
+        userId : "clc4wj5lh3gyi0ak4eq4n8syr",
+        title : "Milwaukee Mission",
+        author : "Elder Cooper",
+        imageUrl : "https://via.placeholder.com/206x260",
+        year : 2021,
+        genre : "Policier",
+        ratings : [{
+        userId : "1",
+        grade: 5
+  },
+    {
+        userId : "1",
+        grade : 5
+    },
+    {
+        userId : "clc4wj5lh3gyi0ak4eq4n8syr",
+        grade : 5
+    },
+    {
+        userId : "1",
+        grade : 5
+    }],
+    averageRating: 3
+      },
+    ];
+    res.status(200).json(stuff);
+  });
 
 module.exports = app;
