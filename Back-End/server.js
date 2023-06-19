8078e644c00caf1a4e218b022045aaa48a73d6d5
Baptiste Ribot <baptisteribot@outlook.fr>
2023-06-19 14:01:24 +0200

    ajout nodemon

--- Back-End/server.js
+++ Back-End/server.js
@@ -4,4 +4,4 @@ const server = http.createServer((req, res) => {
     res.end('Voilà la réponse du serveur');
 });
 
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 4000);
