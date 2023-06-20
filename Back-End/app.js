91dc273551d6d8f8317fbfb97739db716bfa3ed8
Baptiste Ribot <baptisteribot@outlook.fr>
2023-06-20 10:22:02 +0200

    ajout POST pour créer des livres

--- Back-End/app.js
+++ Back-End/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Book = require("./models/Book");
+
+//Se connecter à la base de donnée Atlas
 mongoose
   .connect(
     "mongodb+srv://baptisteribot:PIQlCQMzm1Lg5COi@vieuxgrimoire.qhx7tbn.mongodb.net/?retryWrites=true&w=majority",
@@ -8,12 +11,14 @@ mongoose
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
+//Création de constante
+const app = express();
 const data = "../Front-End/public/data/data.json";
 
-const app = express();
-
+//Méthodes .use :
 app.use(express.json());
 
+//CORS
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -27,13 +32,19 @@ app.use((req, res, next) => {
   next();
 });
 
+//Requête POST pour créer des livres
 app.post("/api/books", (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({
-    message: "Objet créé !",
+  delete req.body.id;
+  const book = new Book({
+    ...req.body,
   });
+  book
+    .save()
+    .then(() => res.status(201).json({ message: "Objet enregistré !" }))
+    .catch((error) => res.status(400).json({ error }));
 });
 
+//Requête GET pour recevoir les livres
 app.get("/api/books", (req, res, next) => {
   const books = [
     {
@@ -46,4 +57,5 @@ app.get("/api/books", (req, res, next) => {
   res.status(200).json(books);
 });
 
+//Exporter la fonction
 module.exports = app;
--- Back-End/models/Book.js
+++ Back-End/models/Book.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+//Création d'un schéma pour la base de données
 const bookSchema = mongoose.Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
@@ -8,4 +9,5 @@ const bookSchema = mongoose.Schema({
   genre: { type: String, required: true },
 });
 
+//Exporter la fonction
 module.exports = mongoose.model("Book", bookSchema);
