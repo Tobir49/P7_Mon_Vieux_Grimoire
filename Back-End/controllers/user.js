ffd117498b444408d6e64c2218dcf9dac545e62c
Baptiste Ribot <baptisteribot@outlook.fr>
2023-06-21 11:39:01 +0200

    signup

--- Back-End/controllers/user.js
+++ Back-End/controllers/user.js
@@ -1,3 +1,26 @@
-exports.signup = (req, res, next) => {};
+const bcrypt = require("bcrypt");
+
+const User = require("../models/User");
+
+exports.signup = (req, res, next) => {
+  //1e étape : hacher le mdp (1 : corps de la requête / 2 : nb tours pour créer mdp haché) :
+  bcrypt
+    .hash(req.body.password, 10)
+    //Récupérer le mdp créé et en faire un User
+    .then((hash) => {
+      const user = new User({
+        email: req.body.email,
+        password: hash,
+      });
+      user
+        //Sauvegarder ce User dans BDD
+        .save()
+        .then(() =>
+          res.status(201).json({ message: "Compte créé avec succès !" })
+        )
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
 
 exports.login = (req, res, next) => {};
